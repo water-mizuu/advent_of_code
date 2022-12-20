50b375bf9e44d6d25479e626124d21a14f514242
water-mizuu <mizu.github.em@gmail.com>
2022-12-20 17:55:48 +0800

    Added day 20 solution

---
+++ bin/2022/day_20/main.dart
@@ -0,0 +1,72 @@
+import "dart:io";
+
+///
+/// Just a wee bit of a hack.
+///
+class Box {
+  final int value;
+  const Box(this.value);
+
+  @override
+  bool operator ==(Object other) => identical(this, other);
+
+  @override
+  int get hashCode => super.hashCode;
+
+  @override
+  String toString() => "$value";
+}
+
+void part1() {
+  List<String> lines = File("bin/2022/day_20/assets/main.txt").readAsLinesSync();
+  List<Box> normalized = lines.map(int.parse).map(Box.new).toList();
+  int length = normalized.length;
+
+  /// Since we need to run them by order, keep track of the indices
+  ///   By keeping a copy.
+  for (Box boxed in normalized.toList()) {
+    int index = normalized.indexOf(boxed);
+
+    normalized.removeAt(index);
+    normalized.insert((index + boxed.value) % (length - 1), boxed);
+  }
+
+  int zeroIndex = normalized.indexWhere((b) => b.value == 0);
+  int sum = normalized[(zeroIndex + 1000) % length].value +
+      normalized[(zeroIndex + 2000) % length].value +
+      normalized[(zeroIndex + 3000) % length].value;
+
+  print(sum);
+}
+
+void part2() {
+  int decryptionKey = 811589153;
+  List<String> lines = File("bin/2022/day_20/assets/main.txt").readAsLinesSync();
+  List<Box> normalized = lines.map(int.parse).map((v) => decryptionKey * v).map(Box.new).toList();
+
+  /// Since we need to run them by order, keep track of the indices
+  ///   By keeping a copy.
+  List<Box> copy = normalized.toList();
+  int length = normalized.length;
+
+  for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < copy.length; ++i) {
+      int index = normalized.indexOf(copy[i]);
+
+      normalized.removeAt(index);
+      normalized.insert((index + copy[i].value) % (length - 1), copy[i]);
+    }
+  }
+
+  int zeroIndex = normalized.indexWhere((b) => b.value == 0);
+  int sum = normalized[(zeroIndex + 1000) % length].value +
+      normalized[(zeroIndex + 2000) % length].value +
+      normalized[(zeroIndex + 3000) % length].value;
+
+  print(sum);
+}
+
+void main() {
+  part1();
+  part2();
+}
